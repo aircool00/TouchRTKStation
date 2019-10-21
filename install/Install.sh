f84414155b69a3553a46be2c7b357201a59669d0
unknown <aircool00@gmail.com>
2019-10-21 18:56:23 -0400

    Added Upgrade to install and Raspberry Pi Pixel-Desktop for installing ontop Rasbian Lite image

--- install/Install.sh
+++ install/Install.sh
@@ -3,9 +3,10 @@
 cd /home/pi/
 
 apt-get update -y
+apt-get upgrade -y
 
 # Install pyqt5
-apt-get install -y qt5-default pyqt5-dev pyqt5-dev-tools
+apt-get install -y qt5-default pyqt5-dev pyqt5-dev-tools raspberrypi-ui-mods
 
 # Install RTKLIB
 git clone -b demo5 https://github.com/rtklibexplorer/RTKLIB.git
