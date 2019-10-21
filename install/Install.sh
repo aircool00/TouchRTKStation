72ec55835add9c4e871e64065b3f46d317d86ee6
unknown <aircool00@gmail.com>
2019-10-21 14:49:30 -0400

    Changed RTKLIB to rtklibexplorer demo5 fork

--- install/Install.sh
+++ install/Install.sh
@@ -8,7 +8,7 @@ apt-get update -y
 apt-get install -y qt5-default pyqt5-dev pyqt5-dev-tools
 
 # Install RTKLIB
-git clone -b rtklib_2.4.3 https://github.com/tomojitakasu/RTKLIB.git
+git clone -b demo5 https://github.com/rtklibexplorer/RTKLIB.git
 cd ./RTKLIB/app/str2str/gcc/
 make
 cd ../../rtkrcv/gcc/
