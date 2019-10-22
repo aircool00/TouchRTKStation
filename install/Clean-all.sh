f6f0b04feffed202c505165bd33f1a8d35f64cf4
unknown <aircool00@gmail.com>
2019-10-22 00:39:37 -0400

    Added local (/usr/local/bin) [str2str,rtkrcv] install of RTKLIB files to be romved in cleaning scripts

--- install/Clean-all.sh
+++ install/Clean-all.sh
@@ -4,5 +4,9 @@ cd /home/pi/
 rm -rf TouchRTKStation
 rm -rf RTKLIB
 sudo rm -rf .config/autostart
+cd ../../
+cd /usr/local/bin
+sudo rm str2str
+sudo rm rtkrcv
 
 reboot
--- install/Clean-rtklib.sh
+++ install/Clean-rtklib.sh
@@ -3,4 +3,7 @@ cd /home/pi/
 
 rm -rf RTKLIB
 sudo rm -rf .config/autostart
+cd /usr/local/bin
+sudo rm str2str
+sudo rm rtkrcv
 
